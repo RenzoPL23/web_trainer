a66fd0fa8efbc20bd73b51af0dd4cd23fe927840
RenzoPL23 <renperez157@gmail.com>
2020-12-19 22:20:07 -0500

    convert to .net web tr4iner

---
+++ web_tr4iner/web_tr4iner/Controllers/PagesController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace web_tr4iner.Controllers
+{
+    public class PagesController : Controller
+    {
+        // GET: Pages
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ web_tr4iner/web_tr4iner/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="web_tr4iner.MvcApplication" Language="C#" %>
